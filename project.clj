b9caf69091594d097aae34fd9f60fe111c744c6f
John Chijioke <johnbendie@gmail.com>
2014-09-24 14:21:32 +0100

    Added detailed project information.

--- project.clj
+++ project.clj
@@ -1,4 +1,13 @@
 (defproject com.bizlogic/service-tools "0.0.1-SNAPSHOT"
   :description "Service tools for bzilogic clojure projects."
+  :url "http://github.com/johnbendi/service-tools"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :source-paths ["src"])
+  :source-paths ["src"]
+  :scm {:url "git@github.com:johnbendi/service-tools.git"}
+  :pom-addition [:developers [:developer
+                              [:name "John Chijioke"]
+                              [:url "http://github.com/johnbendi"]
+                              [:email "johnbendie@gmail.com"]
+                              [:timezone "+1"]]])
